cc2dfccd0f0926e8364e7f00087dc3a479022015
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-19 00:17:31 +0200

    add webhook api

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,36 @@
+
+
+import { NextRequest, NextResponse } from 'next/server';
+import 
+
+
+export async function POST(req: NextRequest) {
+  try {
+    const { postId } = await req.json();
+    const pageAccessToken = process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN; // Store securely
+
+    const response = await fetch(
+      `https://graph.facebook.com/v19.0/${postId}/comments`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          message: `
+          Stream and Download Xhosa Hip Hop Videos on https://www.xhap.co.za
+          Shop & Blog on https://espazza.co.za`,
+          access_token: pageAccessToken,
+        }),
+      }
+    );
+
+    const data = await response.json();
+    if (data.error) throw data.error;
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.log('\n\nError pposting comment:', error);
+    return NextResponse.json(
+      { error: 'Failed to post comment' },
+      { status: 500 }
+    );
+  }
+}
